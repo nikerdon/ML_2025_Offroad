fbc48d00e182de782b2b508754f7f1e0d158adb2
Elizabeth S Gould <beth@gouldhome.com>
2025-05-19 16:00:39 +0300

    Update app_api.py

--- app_files/app_api.py
+++ app_files/app_api.py
@@ -139,3 +139,22 @@ if __name__ == '__main__':
     import uvicorn
     #uvicorn.run(app, host="0.0.0.0", port=5000)
     uvicorn.run(app, host="127.0.0.1", port=5000)
+
+
+
+    #from typing import Annotated
+
+'''from fastapi import FastAPI, File, UploadFile
+
+app = FastAPI()
+
+
+@app.post("/files/")
+async def create_file(file: Annotated[bytes, File()]):
+    return {"file_size": len(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
+'''
